40f165b36a4dd78f78c588118b71a962bfc0ba41
Hồng Chinh <chinhuv@gmail.com>
2019-02-20 16:07:35 +0700

    Initial Commit

---
+++ DemoAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAlamofire
+//
+//  Created by Hồng Chinh on 2/20/19.
+//  Copyright © 2019 Hồng Chinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
