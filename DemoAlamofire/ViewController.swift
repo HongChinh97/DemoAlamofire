604b1e5d4149adda10dd6fee5018a15f004c90bd
Hồng Chinh <chinhuv@gmail.com>
2019-02-20 17:33:33 +0700

    alamofier

--- DemoAlamofire/ViewController.swift
+++ DemoAlamofire/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    @IBOutlet var tblJSON: UITableView!
+    var arrRes = [[String:AnyObject]]() //Array of dictionary 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("http://api.androidhive.info/contacts/").responseJSON{ (responseData) -> Void in
+            if ((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["contacts"].arrayObject {
+                    self.arrRes = resData as! [[String:AnyObject]]
+                }
+                if self.arrRes.count > 0 {
+                    self.tblJSON.reloadData()
+                }
+            }
+        }
     }
 
 
